3c698dc73861b266e23bf44f5786308ee6025e94
Toru Choi <blueocean239@gmail.com>
2017-01-05 17:29:04 +0900

    setonclicklistener and refactoring

--- app/src/main/java/io/toru/daggerinkotlin/model/ItemTypeModel.kt
+++ app/src/main/java/io/toru/daggerinkotlin/model/ItemTypeModel.kt
@@ -3,5 +3,4 @@ package io.toru.daggerinkotlin.model
 /**
  * Created by wonyoung on 2017. 1. 4..
  */
-class ItemTypeModel (val text:String){
-}
+data class ItemTypeModel (val text:String)
--- app/src/main/java/io/toru/daggerinkotlin/ui/adapter/MainAdapter.kt
+++ app/src/main/java/io/toru/daggerinkotlin/ui/adapter/MainAdapter.kt
@@ -11,25 +11,27 @@ import kotlinx.android.synthetic.main.main_adapter.view.*
 /**
  * Created by wonyoung on 2017. 1. 4..
  */
-class MainAdapter(var itemList:List<ItemTypeModel>): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
+class MainAdapter(var itemList:List<ItemTypeModel>, val itemClick: (ItemTypeModel) -> Unit): RecyclerView.Adapter<MainAdapter.MainViewHolder>(){
     override fun getItemCount(): Int {
         return itemList.size
     }
-
-    override fun onBindViewHolder(holder: MainViewHolder?, position: Int) {
-        holder?.update(itemList[position])
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        with(holder){
+            update(itemList[position])
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MainViewHolder {
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.main_adapter, parent, false)
-        return MainViewHolder(view)
+        return MainViewHolder(view, itemClick)
     }
 
-    inner class MainViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+    inner class MainViewHolder(itemView:View, val itemClick:(ItemTypeModel)->Unit) : RecyclerView.ViewHolder(itemView){
 
-        fun update(item:ItemTypeModel?){
+        fun update(item:ItemTypeModel){
             with(item){
-                itemView.main_adapter_text.text= item?.text
+                itemView.main_adapter_text.text = this.text
+                itemView.setOnClickListener { itemClick(this) }
             }
         }
     }
